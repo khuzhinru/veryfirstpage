e23b76b38a05c123ca23bfeac1974a8d32d0f56e
Ruslan Khuzhin Work <khuzh.n@gmail.com>
2021-03-05 18:14:51 +0500

    Started to make new engine adding functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { SearchInput } from "./src/js/classes/SearchInput.js";
 import { SearchEngines } from "./src/js/classes/SearchEngines.js";
 import { defaultSearchEngines } from "./src/js/defaultSearchEngines.js";
 import { Request } from "./src/js/classes/Request.js";
+import { Modal } from "./src/js/classes/Modal.js";
 
 const searchInput = new SearchInput(document.getElementById("searchInput"));
 const searchEngines = new SearchEngines(document.getElementById("searchEnginesContainer"));
@@ -17,3 +18,40 @@ searchEngines.items.forEach((searchEngine) => {
     request.send();
   }
 });
+
+const addSearchEngineModal = new Modal({
+  name: "addSearchEngineModal",
+  title: 'New Search Engine'
+});
+document.body.appendChild(addSearchEngineModal.HTMLElement.container);
+
+/* modal.toggle(); */
+
+addSearchEngineModal.HTMLElement.body.innerHTML = `
+  <form id="addSearchEngineForm" class="form">
+    <div class="form__form-group">
+      <label class="form__label">Name:</label>
+      <input class="form__input input" type="text">
+    </div>
+    <div class="form__form-group">
+      <label class="form__label">Search URL:</label>
+      <input class="form__input input" type="text">
+    </div>
+    <div class="form__form-group">
+      <label class="form__label">Basic URL:</label>
+      <input class="form__input input" type="text">
+    </div>
+  </form>
+`;
+
+addSearchEngineModal.HTMLElement.footer.innerHTML = `
+  <button>Save</button>
+`;
+
+const addSearchEngineBtn = document.createElement('a');
+addSearchEngineBtn.className = 'searchEngine searchEngineAddBtn';
+addSearchEngineBtn.href = '#';
+addSearchEngineBtn.textContent = 'Add new';
+searchEngines.HTMLElement.appendChild(addSearchEngineBtn);
+
+addSearchEngineBtn.onclick = addSearchEngineModal.show;
