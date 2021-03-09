cad5368fbe3692eab1570c753e7fb3314126cc17
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-03-09 23:02:32 +0500

    Installed and configured Webpack; script.js file renamed to index.js and moved to ./src/js folder

---
+++ src/js/index.js
@@ -0,0 +1,55 @@
+import { SearchInput } from "./classes/SearchInput.js";
+import { SearchEngines } from "./classes/SearchEngines.js";
+import { defaultSearchEngines } from "./defaultSearchEngines.js";
+import { Request } from "./classes/Request.js";
+import { Modal } from "./classes/Modal.js";
+
+const searchInput = new SearchInput(document.getElementById("searchInput"));
+const searchEngines = new SearchEngines(document.getElementById("searchEnginesContainer"));
+
+defaultSearchEngines.forEach((searchEngineObj) => {
+  searchEngines.create(searchEngineObj);
+});
+
+searchEngines.items.forEach((searchEngine) => {
+  searchEngine.HTMLElement.onclick = (e) => {
+    e.preventDefault();
+    let request = new Request(searchEngine.searchUrl + searchInput.value);
+    request.send();
+  };
+});
+
+const addSearchEngineModal = new Modal({
+  name: "addSearchEngineModal",
+  title: "New Search Engine",
+});
+document.body.appendChild(addSearchEngineModal.HTMLElement.container);
+
+addSearchEngineModal.HTMLElement.body.innerHTML = `
+  <form id="addSearchEngineForm" class="form">
+    <div class="form__form-group">
+      <label class="form__label">Name:</label>
+      <input class="form__input input" type="text">
+    </div>
+    <div class="form__form-group">
+      <label class="form__label">Search URL:</label>
+      <input class="form__input input" type="text">
+    </div>
+    <div class="form__form-group">
+      <label class="form__label">Basic URL:</label>
+      <input class="form__input input" type="text">
+    </div>
+  </form>
+`;
+
+addSearchEngineModal.HTMLElement.footer.innerHTML = `
+  <button class="btn btn_primary">Save</button>
+`;
+
+const addSearchEngineBtn = document.createElement("a");
+addSearchEngineBtn.className = "searchEngine searchEngineAddBtn";
+addSearchEngineBtn.href = "#";
+addSearchEngineBtn.textContent = "Add new";
+searchEngines.HTMLElement.appendChild(addSearchEngineBtn);
+
+addSearchEngineBtn.onclick = addSearchEngineModal.show;
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/js/index.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
