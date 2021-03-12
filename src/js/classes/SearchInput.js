643372d0f29382dacaf1d04f4f42a0e381511d5e
Ruslan Khuzhin Work <khuzh.n@gmail.com>
2021-03-12 18:02:45 +0500

    Renamed HTMLElement to DOMElement

--- src/js/classes/SearchInput.js
+++ src/js/classes/SearchInput.js
@@ -1,21 +1,24 @@
 export class SearchInput {
   constructor(searchInputHTMLElement) {
     this.value = "";
-    this.HTMLElement = searchInputHTMLElement;
+    this.DOMElement = searchInputHTMLElement;
 
     this.clear = this.clear.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
 
-    this.HTMLElement.onchange = this.handleInputChange;
+    this.setup();
+  }
+
+  setup() {
+    this.DOMElement.addEventListener('change', this.handleInputChange);
   }
 
   clear() {
     this.value = "";
-    this.HTMLElement.value = "";
+    this.DOMElement.value = "";
   }
 
   handleInputChange() {
-    this.value = this.HTMLElement.value;
-    console.log(this.value);
+    this.value = this.DOMElement.value;
   }
 }
