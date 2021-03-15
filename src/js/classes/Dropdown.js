465d2ec342088a13b47988cc7721c076df48287a
Ruslan Khuzhin Work <khuzh.n@gmail.com>
2021-03-15 14:54:16 +0500

    Hide dropdown on click outside

--- src/js/classes/Dropdown.js
+++ src/js/classes/Dropdown.js
@@ -8,8 +8,10 @@ export class Dropdown {
     this.toggle = this.toggle.bind(this);
     this.show = this.show.bind(this);
     this.hide = this.hide.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
     this.trigger.addEventListener("click", this.toggle);
+
   }
 
   toggle() {
@@ -19,10 +21,18 @@ export class Dropdown {
   show() {
     this.menu.classList.add("dropdown__menu_show");
     this.isShow = true;
+    document.addEventListener('click', this.handleClick);
   }
 
   hide() {
     this.menu.classList.remove("dropdown__menu_show");
     this.isShow = false;
+    document.removeEventListener('click', this.handleClick);
+  }
+
+  handleClick(e) {
+    if (!e.target.closest('[data-name="dropdown"]') || e.target.closest('[data-name="dropdownMenu"]')) {
+      this.hide();
+    }
   }
 }
