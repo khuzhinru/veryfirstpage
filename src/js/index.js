dc6e4170ac9c028c61f3bafa574765acdd535d32
Ruslan Khuzhin Work <khuzh.n@gmail.com>
2021-03-24 18:07:52 +0500

    little changes

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import "../sass/style.scss";
 import { blocks } from "./blocks";
 blocks.initAuto();
 
-const searchInput = blocks.init(document.getElementById("searchInput"));
+const searchInput = new blocks.searchInput(document.getElementById("searchInput"));
 const searchEngines = new blocks.searchEngines(document.getElementById("searchEnginesContainer"), { searchInput });
+window.userSearchEngines = searchEngines;
